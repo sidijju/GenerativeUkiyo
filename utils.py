f04b136e0c530790d603d749ba118fc982a49c25
sidijju <sidijju@gmail.com>
2024-04-06 16:51:25 -0700

    add utils.py

---
+++ utils.py
@@ -0,0 +1,30 @@
+import os
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import torchvision.utils as vutils
+
+def make_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def plot_image(image, path):
+    plt.cla()
+    plt.imshow(image.cpu().permute(1, 2, 0))
+    plt.savefig(path)
+
+def plot_batch(batch, path):
+    plt.cla()
+    grid = vutils.make_grid(batch.cpu()[:25], nrow = 5, padding=2, normalize=True)
+    plt.axis('off')
+    plt.imshow(grid.permute(1, 2, 0))
+    plt.savefig(path)
+
+# utility function to iterate through model
+# and initalize weights in layers rom N(0, 0.02)
+def weights_init(model):
+    classname = model.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(model.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(model.weight.data, 1.0, 0.02)
+        nn.init.constant_(model.bias.data, 0)
