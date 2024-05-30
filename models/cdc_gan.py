7e396da21d5f31ebd5f2dc4655affe606dcee4eb
sidijju <sidijju@gmail.com>
2024-05-30 17:33:29 -0600

    change GAN file names

--- models/cdc_gan.py
+++ models/cdc_gan.py
@@ -3,19 +3,18 @@
 # https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
 # https://arxiv.org/abs/1606.03498
 #####################
+import os
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-import torch.nn.utils.parametrizations as P
 import torch.optim as optim
+import torchvision.utils as vutils
 from tqdm import tqdm
 from datetime import datetime
 
-from utils import *
+##### CDCGAN #####
 
-##### DCGAN #####
-
-class DCGAN:
+class CDCGAN:
 
     def __init__(self, 
                 args,
@@ -30,30 +29,31 @@ class DCGAN:
         self.channel_size = channel_size
         self.latent_size = args.latent
 
-        if self.args.train:
-            self.run_dir = "train/gan-" + datetime.now().strftime("%Y-%m-%d(%H:%M:%S)" + "/")
-            self.progress_dir = self.run_dir + "progress/"
-            make_dir(self.run_dir)
-            make_dir(self.progress_dir)
+        self.run_dir = "train/" + datetime.now().strftime("%Y-%m-%d(%H:%M:%S)" + "/")
+        self.progress_dir = self.run_dir + "progress/"
+        if not os.path.exists(self.run_dir):
+            os.makedirs(self.run_dir)
+        if not os.path.exists(self.progress_dir):
+            os.makedirs(self.progress_dir)
         
     def train(self, 
             num_epochs = 5,
-            g_lr = .0001,
-            d_lr = .0004):
+            lr = .0002):
 
-        assert self.args.train and self.dataloader
-        
+        if not self.dataloader:
+            return 
+            
         d_net = Discriminator(self.args, self.channel_size)
-        d_net.apply(weights_init)
+        d_net.apply(self.weights_init)
         d_net.to(self.args.device)
-        d_optimizer = optim.Adam(d_net.parameters(), lr=d_lr, betas=(0.5, 0.999))
-        if self.args.fm_on:
-            d_net.model.register_forward_hook(d_net.feature_activations)
+        d_optimizer = optim.Adam(d_net.parameters(), lr=lr, betas=(0.5, 0.999))
+        if self.args.fm:
+            d_net.l1.register_forward_hook(d_net.feature_activations)
 
         g_net = Generator(self.args, self.channel_size, self.latent_size)
-        g_net.apply(weights_init)
+        g_net.apply(self.weights_init)
         g_net.to(self.args.device)
-        g_optimizer = optim.Adam(g_net.parameters(), lr=g_lr, betas=(0.5, 0.999))
+        g_optimizer = optim.Adam(g_net.parameters(), lr=lr, betas=(0.5, 0.999))
 
         mse = nn.MSELoss()
         bce = nn.BCELoss()
@@ -64,6 +64,7 @@ class DCGAN:
         real_label = 0.9
         fake_label = 0.
 
+        images = []
         d_losses_real = []
         d_losses_fake = []
         g_losses = []
@@ -78,11 +79,14 @@ class DCGAN:
                 batchsize = batch.shape[0]
 
                 if iters == 0:
-                    plot_batch(batch, self.progress_dir + f"train_example")
+                    images.append(vutils.make_grid(batch.cpu(), padding=2, normalize=True))
+                    plt.axis('off')
+                    plt.imshow(images[-1].permute(1, 2, 0))
+                    plt.savefig(self.progress_dir + f"train_example")
 
                 # generate fake batch for training
                 noise = torch.randn(batchsize, self.latent_size, 1, 1, device=self.args.device)
-                fake_batch = g_net(noise)
+                fake_batch = g_net(noise, labels)
 
                 real_labels = torch.full((batchsize,), real_label, dtype=torch.float, device=self.args.device)
                 fake_labels = torch.full((batchsize,), fake_label, dtype=torch.float, device=self.args.device)
@@ -94,8 +98,8 @@ class DCGAN:
                 d_net.zero_grad()
 
                 # loss on real inputs
-                output = d_net(batch).view(-1)
-                if self.args.fm_on:
+                output = d_net(batch, labels).view(-1)
+                if self.args.fm:
                     fx = d_net.features.detach().clone()
                 d_loss_real = bce(output, real_labels)
                 d_loss_real.backward()
@@ -103,7 +107,7 @@ class DCGAN:
                 dx = output.mean().item()
 
                 # loss on fake inputs
-                output = d_net(fake_batch.detach()).view(-1)
+                output = d_net(fake_batch.detach(), labels).view(-1)
                 d_loss_fake = bce(output, fake_labels)
                 d_loss_fake.backward()
                 # D(G(z))
@@ -116,22 +120,17 @@ class DCGAN:
                 #############################
 
                 g_net.zero_grad()
-                output = d_net(fake_batch).view(-1)
+                output = d_net(fake_batch, labels).view(-1)
 
-                if self.args.fm_on:
+                if self.args.fm:
                     # Feature matching objective from "Improved Techniques for Training GANs"
                     e_fx = fx.mean(dim=0)
                     fgz = d_net.features
                     e_fgz = fgz.mean(dim=0)
                     g_loss = torch.square(mse(e_fx, e_fgz))
                 else:
-                    # Flip label option for better gradient flow
-                    if self.args.flip:
-                        g_loss = bce(output, fake_labels)
-                    else:
-                        g_loss = bce(output, real_labels)
-                
-                # to check for vanishing gradients or exploding gradients
+                    g_loss = bce(output, real_labels)
+
                 g_loss.backward()
                 # D(G(z))
                 dgz_2 = output.mean().item()
@@ -145,88 +144,100 @@ class DCGAN:
                 if i % 100 == 0:
                     print(f'[%d/%d][%d/%d]\td_loss: %.4f\tg_loss: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
                         % (epoch, num_epochs, i, len(self.dataloader),
-                            (d_loss_real.item() + d_loss_fake.item())/2, g_loss.item(), dx, dgz_1, dgz_2))
+                            d_loss_real.item() + d_loss_fake.item(), g_loss.item(), dx, dgz_1, dgz_2))
 
                 d_losses_real.append(d_loss_real.item())
                 d_losses_fake.append(d_loss_fake.item())
                 g_losses.append(g_loss.item())
 
-                if (iters % 5000 == 0) or ((epoch == num_epochs-1) and (i == len(self.dataloader)-1)):
-
+                if (iters % 1000 == 0) or ((epoch == num_epochs-1) and (i == len(self.dataloader)-1)):
                     with torch.no_grad():
-                        fake = g_net(fixed_latent).detach()
-                    plot_batch(fake, self.progress_dir + f"iter:{iters}")
+                        fake = g_net(fixed_latent, labels).detach().cpu()
+                    images.append(vutils.make_grid(fake, padding=2, normalize=True))
+                    plt.axis('off')
+                    plt.imshow(images[-1].permute(1, 2, 0))
+                    plt.savefig(self.progress_dir + f"iter:{iters}")
 
                 iters += 1
-
         print("### End Training Procedure ###")
         self.save_train_data(d_losses_real, d_losses_fake, g_losses, d_net, g_net)
+
 
     def save_train_data(self, d_losses_real, d_losses_fake, g_losses, d_net, g_net):
 
         # save models
-        torch.save(d_net.state_dict(), self.run_dir + '/discriminator.pt')
-        torch.save(g_net.state_dict(), self.run_dir + '/generator.pt')
+        torch.save(d_net.state_dict(), self.run_dir + "discriminator")
+        torch.save(g_net.state_dict(), self.run_dir + "generator")
 
         # save losses
         plt.figure(figsize=(10,5))
         plt.title("Training Losses")
         plt.plot(g_losses,label="G")
-        plt.plot([sum(x)/2 for x in zip(d_losses_real, d_losses_fake)], label="D")
+        plt.plot([sum(x) for x in zip(d_losses_real, d_losses_fake)], label="D")
+        #plt.plot(d_losses_real, label="D(x)")
+        #plt.plot(d_losses_fake, label="D(G(z))")
         plt.xlabel("Iterations")
         plt.ylabel("Loss")
         plt.legend()
         plt.savefig(self.run_dir + "train_losses")
                 
-    def generate(self, path, n = 5):
-        g_net = Generator(self.args, self.channel_size, self.latent_size)
-        g_net.load_state_dict(torch.load(path + "/generator.pt"))
-        g_net.to(self.args.device)
-        g_net.eval()
+    def generate(self, n = 5):
+        pass
 
-        noise = torch.randn(n, self.latent_size, 1, 1, device=self.args.device)
-
-        batch, _ = next(iter(self.dataloader))
-        batch = batch.to(self.args.device)
-
-        with torch.no_grad():
-            fake = g_net(noise)
-
-        for i in range(n):
-            plot_image(batch[i], path + f"/r_{i}")
-            plot_image(fake[i], path + f"/f_{i}")
+    # utility function to iterate through model
+    # and initalize weights in layers rom N(0, 0.02)
+    def weights_init(self, model):
+        classname = model.__class__.__name__
+        if classname.find('Conv') != -1:
+            nn.init.normal_(model.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            nn.init.normal_(model.weight.data, 1.0, 0.02)
+            nn.init.constant_(model.bias.data, 0)
+        elif classname.find('ConvTranspose2d') != -1:
+            nn.init.normal_(model.weight.data, 0.0, 0.02)
+            nn.init.constant_(model.bias.data, 0)
 
 ###############
             
-####   Generator   #####
+#####   Generator   #####
 
 class Generator(nn.Module):
     def __init__(self, args, channel_size, latent_size):
         super(Generator, self).__init__()
         self.args = args
 
-        ngf = 64
+        self.embed = self.conv_block(latent_size, 1024, 4, stride=1, pad=0)
+        self.label_embedding = self.conv_block(self.args.num_classes, 1024, 4, stride=1, pad=0)
 
         self.model = nn.Sequential(
-            self.conv_block(latent_size, ngf * 16, 4, stride=1, pad=0),
-            #self.conv_block(ngf * 32, ngf * 16, 4),
-            self.conv_block(ngf * 16, ngf * 8, 4),
-            self.conv_block(ngf * 8, ngf * 4, 4),
-            self.conv_block(ngf * 4, ngf * 2, 4),
-            self.conv_block(ngf * 2, ngf * 1, 4),
-            P.spectral_norm(nn.ConvTranspose2d(ngf, channel_size, 4, 2, 1, bias=False)),
+            # shape: (2048, 4, 4)
+            self.conv_block(2048, 1024, 4),
+            # shape: (1024, 8, 8)
+            self.conv_block(1024, 512, 4),
+            # shape: (512, 16, 16)
+            self.conv_block(512, 256, 4),
+            # shape: (256, 32, 32)
+            self.conv_block(256, 128, 4),
+            # shape: (128, 64, 64)
+            nn.ConvTranspose2d(128, channel_size, 4, 2, 1, bias=False),
             nn.Tanh()
+            # shape: (3, 128, 128)
         )
 
     def conv_block(self, input, output, kernel, stride=2, pad=1):
         return nn.Sequential(
-            P.spectral_norm(nn.ConvTranspose2d(input, output, kernel, stride, pad, bias=False)),
+            nn.ConvTranspose2d(input, output, kernel, stride, pad, bias=False),
             nn.BatchNorm2d(output),
             nn.ReLU(True),
         )
 
-    def forward(self, input):
-        return self.model(input)
+    def forward(self, input, label):
+        input_embed = self.embed(input)
+        one_hot_label = nn.functional.one_hot(label, num_classes=self.args.num_classes)
+        one_hot_label = one_hot_label.unsqueeze(-1).unsqueeze(-1).float()
+        label_embed = self.label_embedding(one_hot_label)
+        label_input = torch.cat((input_embed, label_embed), dim=1)
+        return self.model(label_input)
     
 #########################
     
@@ -237,37 +248,55 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.args = args
 
-        ndf = 64
-
-        self.model = nn.Sequential(
-            self.conv_block(channel_size, ndf, 4, batchnorm=False),
-            self.conv_block(ndf, ndf * 2, 4),
-            self.conv_block(ndf * 2, ndf * 4, 4),
-            self.conv_block(ndf * 4, ndf * 8, 4),
-            self.conv_block(ndf * 8, ndf * 16, 4),
-            #self.conv_block(ndf * 16, ndf * 32, 4),
+        self.label_embedding = nn.Sequential(
+            nn.Linear(self.args.num_classes, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, 128 * 128),
+            nn.Unflatten(1, (1, 128, 128)),
+            nn.LeakyReLU(0.2, inplace=True),
         )
 
-        self.conv = P.spectral_norm(nn.Conv2d(ndf * 16, 1, 4, 1, 0, bias=False))
-        self.sig = nn.Sigmoid()
+        self.l1 = nn.Sequential(
+            # shape: (4, 128, 128)
+            self.conv_block(channel_size + 1, 64, 4, batchnorm=False),
+            # shape: (64, 64, 64)
+            self.conv_block(64, 128, 4),
+            # shape: (128, 32, 32)
+        )
+        self.l2 = nn.Sequential(
+            self.conv_block(128, 256, 4),
+            # shape: (256, 16, 16)
+            self.conv_block(256, 512, 4),
+            # shape: (512, 8, 8)
+            self.conv_block(512, 1024, 4),
+            # shape: (1024, 4, 4)
+            nn.utils.spectral_norm(nn.Conv2d(1024, 1, 4, 1, 0, bias=False)),
+            nn.Sigmoid()
+            # shape: (1024, 1, 1)
+        )
 
     def conv_block(self, input, output, kernel, batchnorm=True):
         if batchnorm:
             return nn.Sequential(
-                P.spectral_norm(nn.Conv2d(input, output, kernel, 2, 1, bias=False)),
+                nn.utils.spectral_norm(nn.Conv2d(input, output, kernel, 2, 1, bias=False)),
                 nn.BatchNorm2d(output),
                 nn.LeakyReLU(0.2, inplace=True),
             )
         else:
             return nn.Sequential(
-                P.spectral_norm(nn.Conv2d(input, output, kernel, 2, 1, bias=False)),
+                nn.utils.spectral_norm(nn.Conv2d(input, output, kernel, 2, 1, bias=False)),
                 nn.LeakyReLU(0.2, inplace=True),
             )
     
     def feature_activations(self, model, input, output):
         self.features = output
     
-    def forward(self, input):
-        return self.sig(self.conv(self.model(input)))
+    def forward(self, input, label):
+        one_hot_label = nn.functional.one_hot(label, num_classes=self.args.num_classes)
+        one_hot_label = one_hot_label.float()
+        label_channel = self.label_embedding(one_hot_label)
+        label_input = torch.cat((label_channel, input), dim=1)
+        l1 = self.l1(label_input)
+        return self.l2(l1)
     
 #########################
