307d61439869e4f76868957e2a6d109350cd0017
sidijju <sidijju@gmail.com>
2024-06-25 17:21:56 -0600

    fix lr scheduler

--- models/vae.py
+++ models/vae.py
@@ -42,7 +42,7 @@ class VAE:
             vae.apply(weights_init)
         vae.to(self.args.device)
         optimizer = optim.Adam(vae.parameters(), lr=self.args.lr, betas=(0.5, 0.999))
-        scheduler = optim.ReduceLROnPlateau(optimizer, 'min', patience=5)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5)
 
         sample_batch, _ = next(iter(self.dataloader))
         sample_batch = sample_batch.to(self.args.device)
