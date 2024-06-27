afaf6980a001bf1e6b45a5418ccc7cc65f67bf92
sidijju <sidijju@gmail.com>
2024-06-27 14:21:46 -0600

    change worker iterations length

--- worker.sh
+++ worker.sh
@@ -5,4 +5,4 @@ PARENT_DIR="$(dirname $PWD)"
 EXEC_DIR=$PWD
 log_dir="logs/lr=${learning_rate}_beta=${beta}"
 echo "Current working directory is: $(pwd)"
-python main.py --n 8 --lr ${learning_rate} --beta ${beta} --log_dir=${log_dir} --vae --checkpoint checkpoints/vae-n25-beta0.pt
+python main.py --n 10 --lr ${learning_rate} --beta ${beta} --log_dir=${log_dir} --vae --checkpoint checkpoints/vae-n25-beta0.pt
