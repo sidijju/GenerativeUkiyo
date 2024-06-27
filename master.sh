4b66d51fdc721b21e2744173e303f9b1f5bc5108
sidijju <sidijju@gmail.com>
2024-06-26 19:45:45 -0600

    change hyperparam search space 2

--- master.sh
+++ master.sh
@@ -1,6 +1,6 @@
-for learning_rates in .000001 .00001 .00005 .0001
+for learning_rates in .000001 .00001 .0001
 do
-  for betas in .0000001 .000001 .00001
+  for betas in .0000001 .000001 .000005 .00001
   do
     ./worker.sh $learning_rates $betas
   done
