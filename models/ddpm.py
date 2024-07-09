13b3e44caae5f7ae50aa4e24d058c21adf7cf54b
sidijju <sidijju@gmail.com>
2024-07-09 23:07:59 +0100

    change self attention implementation ddpm

--- models/ddpm.py
+++ models/ddpm.py
@@ -281,11 +281,20 @@ class SelfAttention(nn.Module):
         self.to_out = nn.Conv2d(hidden_dim, dim, 1)
 
     def forward(self, x):
+        _, _, h, w = x.shape
         x = self.norm(x)
         qkv = self.to_qkv(x).chunk(3, dim = 1)
         q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> b h (x y) c', h = self.heads), qkv)
-        attn = F.scaled_dot_product_attention(q, k, v, scale=self.scale)
-        return self.to_out(attn)
+        
+        q = q.softmax(dim=-2)
+        k = k.softmax(dim=-1)
+
+        q = q * self.scale
+        context = torch.einsum("b h d n, b h e n -> b h d e", k, v)
+
+        out = torch.einsum("b h d e, b h d n -> b h e n", context, q)
+        out = rearrange(out, "b h c (x y) -> b (h c) x y", h=self.heads, x=h, y=w)
+        return self.to_out(out)
 
 class ResnetBlock(nn.Module):
     def __init__(self, in_channels, out_channels, time_embedding_dim):
