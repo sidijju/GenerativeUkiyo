85e0cb7d86f11068214816bed131d30543261922
sidijju <sidijju@gmail.com>
2024-07-11 20:19:10 +0100

    clean dataset transforms

--- data/dataset.py
+++ data/dataset.py
@@ -9,7 +9,6 @@ class JapArtDataset(Dataset):
     def __init__(self, args, transform=None):
         self.img_dir = args.augment if args.augment else 'data/jap-art/'
         self.dim = args.dim
-        self.transform = transform
 
         self.img_names = []
         self.labels = []
@@ -30,6 +29,13 @@ class JapArtDataset(Dataset):
             self.img_names.append(f)
             self.labels.append(label)
 
+        self.transform = transform if transform else v2.Compose([
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Resize(self.dim),
+            v2.Normalize([0.5], [0.5]),
+            v2.Lambda(lambda x: torch.clamp(x, 0, 1)),
+        ])
+
     def __len__(self):
         return len(self.img_names)
 
@@ -38,40 +44,10 @@ class JapArtDataset(Dataset):
         img = read_image(self.img_names[idx])
 
         assert img.shape[0] <= 3
-
-        if self.transform:
-            img = self.transform(img)
-        else:            
-            # resize so maximum dimension is 720
-            c, w, h = img.size()
-            pad = None
-            if h > w:
-                w = int(w * 720 / h)
-                if w % 2 != 0:
-                    w += 1
-                h = 720
-
-                pad = [0, (720 - w)//2]
-            elif w > h:
-                h = int(h * 720/w)
-                if h % 2 != 0:
-                    h += 1
-                w = 720
-                pad = [(720 - h)//2, 0]
-            else:
-                pad = 0
-
-            if c < 3:
-                img = torch.cat((img, img, img), dim=0)
+        if img.shape[0] < 3:
+            img = torch.cat((img, img, img), dim=0)
             
-            transform = v2.Compose([
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Resize(size=(w, h)),
-                v2.Pad(pad, fill=1),
-                v2.Resize(self.dim),
-            ])
-            img = transform(img)
-            img = torch.clamp(img, 0, 1)
+        img = self.transform(img)
 
         return img, label
     
