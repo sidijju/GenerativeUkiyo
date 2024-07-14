1301188c6b00e2602fdb4162f43a833178cf3dae
sidijju <sidijju@gmail.com>
2024-07-14 18:24:10 +0100

    remove normalization

--- data/dataset.py
+++ data/dataset.py
@@ -32,7 +32,6 @@ class JapArtDataset(Dataset):
         self.transform = transform if transform else v2.Compose([
             v2.ToDtype(torch.float32, scale=True),
             v2.Resize(self.dim),
-            v2.Normalize([0.5], [0.5]),
             v2.Lambda(lambda x: torch.clamp(x, 0, 1)),
         ])
 
