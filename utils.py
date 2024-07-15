a77059c3f6069e499374d15c66b0eca72f951405
sidijju <sidijju@gmail.com>
2024-07-15 23:07:06 +0100

    update scale_0_1 utils

--- utils.py
+++ utils.py
@@ -14,10 +14,8 @@ def extract(t):
     return t
 
 def scale_0_1(image):
-    # scale anything to 0 to 1
-    image_min = torch.min(image[:, :, :, torch.min(image, dim=3)[1]], dim=2)[0]
-    image_max = torch.max(image[:, :, :, torch.max(image, dim=3)[1]], dim=2)[0]
-    return (image - image_min) / (image_max - image_min)
+    # scale any Tensor to 0 to 1
+    return v2.ToDtype(torch.float32, scale=True)(image)
 
 def scale_minus1_1(image):
     # scale 0 to 1 to -1 to 1
