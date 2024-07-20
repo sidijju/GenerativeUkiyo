184b668a5c9573036ca104a76dc248cff30fd3fe
sidijju <sidijju@gmail.com>
2024-07-20 19:55:33 +0100

    remove *.sh scripts

--- master.sh
+++
@@ -1,7 +0,0 @@
-for learning_rates in .0001
-do
-  for betas in .0000001 .0000005 .000001 .000005 .00001
-  do
-    ./worker.sh $learning_rates $betas
-  done
-done
--- worker.sh
+++
@@ -1,8 +0,0 @@
-learning_rate=$1
-beta=$2
-
-PARENT_DIR="$(dirname $PWD)"
-EXEC_DIR=$PWD
-log_dir="logs/lr=${learning_rate}_beta=${beta}"
-echo "Current working directory is: $(pwd)"
-python main.py --n 10 --lr ${learning_rate} --beta ${beta} --log_dir=${log_dir} --vae --checkpoint checkpoints/vae-n25-beta0.pt
