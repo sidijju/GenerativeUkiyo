bf987d51cee9744d8e84d03e3d8067efa7e9f3d0
sidijju <sidijju@gmail.com>
2024-07-26 12:16:13 -0600

    add vq_vae args

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import os
-import glob
 import torch
 import random
 import argparse
-from torchvision.transforms import v2
-from torchvision.io import read_image
-from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 
 from models.dc_gan import DCGAN
 from models.vae import VAE
+from models.vq_vae import VQVAE
 from models.ddpm import DDPM
 from data.dataset import *
-
-from utils import make_dir
 
 parser = argparse.ArgumentParser()
 
@@ -22,7 +16,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--n', type=int, default=5, help='number of training epochs')
 parser.add_argument('--seed', type=int, default=128, help='manual random seed')
 parser.add_argument('--batchsize', type=int, default=32, help='batch size')
-parser.add_argument('--latent', type=int, default=512, help='size of latent dimension')
 parser.add_argument('--dim', type=int, default=128, help='image dimension for input and output')
 parser.add_argument('--lr', type=float, default=1e-6, help='learning rate for training')
 parser.add_argument('--decay', type=float, default=0.9999, help='weight decay for EMA')
@@ -31,6 +24,7 @@ parser.add_argument('--log_dir', type=str, default=None, help='log dir for train
 ### Model Flags
 
 parser.add_argument('--vae', action='store_true', help='train vae model')
+parser.add_argument('--vq_vae', action='store_true', help='train vq-vae model')
 parser.add_argument('--ddpm', action='store_true', help='train denoising diffusion probablistic model')
 
 parser.add_argument('--checkpoint', type=str, default=None, help='train model from checkpoint')
@@ -54,6 +48,11 @@ parser.add_argument('--test_n', type=int, default=16, help='number of images to 
 parser.add_argument('--beta', type=float, default=.0001, help='beta coefficient for KL term in VAE loss')
 parser.add_argument('--mse', default=False, action='store_true', help='flag for MSE loss in VAE')
 parser.add_argument('--annealing', default=False, action='store_true', help='flag for beta annealing in VAE loss')
+parser.add_argument('--latent', type=int, default=512, help='size of latent dimension')
+
+### VQVAE
+
+parser.add_argument('--k', type=int, default=512, help='embedding dimensionality K for VQ-VAE')
 
 ### DDPM
 
@@ -95,6 +94,8 @@ if args.ddpm:
     model = DDPM(args, dataloader)
 elif args.vae:
     model = VAE(args, dataloader)
+elif args.vq_vae:
+    model = VQVAE(args, dataloader)
 else:
     model = DCGAN(args, dataloader)
 
